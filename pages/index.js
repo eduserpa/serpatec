efe2ac88e82352df983c1c0c6f0bd5a90f5b852e
Eduardo Serpa <eduardo.m.serpa@gmail.com>
2021-07-23 13:42:59 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Teste(){
 return <iframe src="https://discord.com/widget?id=734919074961817610&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
 }
-export default Teste
-
 function Teste2(){
 
     return <p>Teste de paragrafo</p>
 }
-export default Teste2
+
+export default Teste Teste2
+
