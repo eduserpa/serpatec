6418f7c83d034878e9b3d4b54b8f7d2933971657
Eduardo Serpa <eduardo.m.serpa@gmail.com>
2021-07-23 19:18:57 -0300

    primeira página

--- pages/Users.js
+++ pages/Users.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-function Users(){
-    return 
-    <body>
-        <p>Users</p>
-        <img src="https://scontent.fvcp3-1.fna.fbcdn.net/v/t1.6435-9/85176563_1423127227847773_1485732726463004672_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=oDe8wIDuNWwAX-TkaPW&_nc_ht=scontent.fvcp3-1.fna&oh=01ec52c9b12e2beea93647949abead4f&oe=611FF92E" alt="marcus" />
-        <iframe src="https://discord.com/widget?id=734919074961817610&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+function Users() {
+   return(
+        <body>
+            <p>Users</p>
+            <img src="https://scontent.fvcp3-1.fna.fbcdn.net/v/t1.6435-9/85176563_1423127227847773_1485732726463004672_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=oDe8wIDuNWwAX-TkaPW&_nc_ht=scontent.fvcp3-1.fna&oh=01ec52c9b12e2beea93647949abead4f&oe=611FF92E" alt="marcus" />
+            <iframe src="https://discord.com/widget?id=734919074961817610&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
 
-    </body>
+        </body>
+   )
 }
-export default Users
 
+
+export default Users  
