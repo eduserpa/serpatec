a47e22de1f58569ea468949fe0feb0efecb7a7e3
Eduardo Serpa <eduardo.m.serpa@gmail.com>
2021-07-23 20:27:30 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,20 @@ import React from "react";
 
 function Home(){
     return (
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8"/>
+            <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <title>SERPATEC</title>
+            <link rel="shortcut icon" href="C:\serpatec\assets\favicon.ico"/>
+        </head>
         <body>
-          <iframe src="https://discord.com/widget?id=734919074961817610&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-          
+          <iframe class="discord_widget" src="https://discord.com/widget?id=734919074961817610&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+
              <a href="https://serpatec.vercel.app/Users"><button type="button" id="folcks" class="folcks">Folcks</button></a>
         </body>
+        </html>
         )
     }
 export default Home
