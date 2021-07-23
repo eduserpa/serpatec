cf4b39e19b634f9c3015260cc04f34f34b52bb9a
Eduardo Serpa <eduardo.m.serpa@gmail.com>
2021-07-23 19:50:27 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import React from "react";
 function Home(){
     return (
         <body>
-             <iframe src="https://discord.com/widget?id=734919074961817610&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-
+          <iframe src="https://discord.com/widget?id=734919074961817610&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+          
              <a href="https://serpatec.vercel.app/Users"><button type="button" id="folcks" class="folcks">Folcks</button></a>
         </body>
         )
