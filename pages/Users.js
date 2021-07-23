45bd83000455f79445674f9f0e473b8c2cd3e4f9
Eduardo Serpa <eduardo.m.serpa@gmail.com>
2021-07-23 19:45:49 -0300

    primeira página

--- pages/Users.js
+++ pages/Users.js
@@ -3,11 +3,10 @@ import React from "react";
 function Users() {
    return(
         <body>
-            <p>Users</p>
+            <h1>Users</h1>
             <img src="https://scontent.fvcp3-1.fna.fbcdn.net/v/t1.6435-9/85176563_1423127227847773_1485732726463004672_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=oDe8wIDuNWwAX-TkaPW&_nc_ht=scontent.fvcp3-1.fna&oh=01ec52c9b12e2beea93647949abead4f&oe=611FF92E" alt="marcus" />
+            <img src="https://scontent.fvcp3-1.fna.fbcdn.net/v/t1.6435-9/72046067_2412582338855611_8256180371419299840_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=uBAzK_YuozUAX8agBfU&_nc_ht=scontent.fvcp3-1.fna&oh=547ffb97e397b5d92b0270c34d8d63c9&oe=611F9D60" alt="danilo" />
         </body>
    )
 }
-
-
 export default Users  
