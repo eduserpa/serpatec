0bdc4d8d331172b67e6e5e4040beae7358c45f4c
Eduardo Serpa <eduardo.m.serpa@gmail.com>
2021-07-23 12:46:39 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return<div>Folcks XD</div>
+    return<iframe src="https://discord.com/widget?id=734919074961817610&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
 
 }
 
