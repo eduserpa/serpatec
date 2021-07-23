b8a48aec65d5a8d5b72f35baa2ee0880084fb914
Eduardo Serpa <eduardo.m.serpa@gmail.com>
2021-07-23 19:24:54 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 
 function Home(){
-    return <body>Home</body>
-}
+    return (
+        <body>
+             <iframe src="https://discord.com/widget?id=734919074961817610&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+        </body>
+        )
+    }
 export default Home
 
