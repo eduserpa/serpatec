9f312471b288cf89e6d5b1f7a06291c4ab14841e
Eduardo Serpa <eduardo.m.serpa@gmail.com>
2021-07-24 12:08:17 -0300

    repage

---
+++ pages/index.off.js
@@ -0,0 +1,13 @@
+import Layout from '../components/layout'
+import Sidebar from '../components/sidebar'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+  )    
+}
